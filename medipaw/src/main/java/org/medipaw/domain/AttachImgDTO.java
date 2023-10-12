71119a9316d75564566bc15d8a42a638435bca6d
kdonghwi99 <dh2268@naver.com>
2023-10-12 12:16:12 +0900

    [수정 테스트]

--- medipaw/src/main/java/org/medipaw/domain/AttachImgDTO.java
+++ medipaw/src/main/java/org/medipaw/domain/AttachImgDTO.java
@@ -7,5 +7,5 @@ public class AttachImgDTO {
 	private String fileName;	// 파일명
 	private String upFolder;	// 폴더 경로
 	private String uuid;		// 중복 방지 아이디
-	// private boolean image;	// 이미지 여부
+	// private boolean image;	// 이미지 여부부부
 }
